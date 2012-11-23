7e0daf04529431c3c810114ee08cad4bb8c8a5f8
Albert Ng <alngz89@gmail.com>
2012-11-22 18:58:51 -0800

    Added RamModule Preload() function
    Added a Preload() function to Ram Module class
    Changed Input Reader to use Preload() in the constructor
    Changed some integer types in Input Reader to match the file generation
    tools
    Changed Input Reader and Ram Module unit tests to use and test Preload()

--- simulator/test/ram_module_test.cpp
+++ simulator/test/ram_module_test.cpp
@@ -8,34 +8,63 @@
 #define RAM_ADDRESS_WIDTH 5
 #define RAM_LATENCY 2
 
-// Helper function to preload RAMs with incrementing values
-void preload (RamModule<int>* rm) {
-  Ram<int>** rams = rm->rams();
-  
-  for (int i = 0; i < NUM_RAMS; i++) {
-    for (int j = 0; j < (int) pow(2, RAM_ADDRESS_WIDTH); j++) {
-      rams[i]->WriteRequest(j, (i << RAM_ADDRESS_WIDTH) + j);
-      rams[i]->NextClockCycle();
-      rams[i]->NextClockCycle();
-    }
-  }
-}
-
 int main (int argc, char** argv) {
   RamModule<int> rm(NUM_RAMS, NUM_PORTS, RAM_ADDRESS_WIDTH, RAM_LATENCY);
+  unsigned int ram_size = (unsigned int) pow(2, RAM_ADDRESS_WIDTH);
+  unsigned int ram_module_size = NUM_RAMS * ram_size;
+  int* test_values = new int[ram_module_size];
+  for (unsigned int i = 0; i < ram_module_size; i++) {
+    test_values[i] = i;
+  }
   
   // Reset and preload RAM module
   rm.Reset();
-  preload(&rm);
+  rm.Preload(test_values, ram_module_size, false);
   
   // Check initial state
   for (int i = 0; i < NUM_PORTS; i++) {
     assert(rm.IsPortReady(i) == true);
     assert(rm.ReadReady(i) == false);
   }
-  assert(rm.Size() == NUM_RAMS * pow(2, RAM_ADDRESS_WIDTH));
+  assert(rm.Size() == ram_module_size);
+  
+  // Single port non-interleaving full readout test
+  for (unsigned int i = 0; i < ram_module_size; i++) {
+    rm.ReadRequest(i, 0);
+    if (i >= RAM_LATENCY + 2) {
+      unsigned int test_addr = i - (RAM_LATENCY + 2);
+      assert(rm.ReadReady(0) == true);
+      assert(rm.ReadData(0) == (int) (test_addr));
+    }
+    rm.NextClockCycle();
+  }
+  for (unsigned int i = ram_module_size - (RAM_LATENCY + 2); i < ram_size; i++) {
+    assert(rm.ReadReady(0) == true);
+    assert(rm.ReadData(0) == (int) i);
+    rm.NextClockCycle();
+  }
+  
+  // Single port interleaving full readout test
+  rm.Reset();
+  rm.Preload(test_values, ram_module_size, true);
+  for (unsigned int i = 0; i < ram_module_size; i++) {
+    rm.ReadRequest(i, 0);
+    if (i >= RAM_LATENCY + 2) {
+      unsigned int test_addr = i - (RAM_LATENCY + 2);
+      assert(rm.ReadReady(0) == true);
+      assert(rm.ReadData(0) == (int) ((test_addr % ram_size) * NUM_RAMS + (test_addr / ram_size)));
+    }
+    rm.NextClockCycle();
+  }
+  for (unsigned int i = ram_module_size - (RAM_LATENCY + 2); i < ram_module_size; i++) {
+    assert(rm.ReadReady(0) == true);
+    assert(rm.ReadData(0) == (int) ((i % ram_size) * NUM_RAMS + (i / ram_size)));
+    rm.NextClockCycle();
+  }
   
   // Multi-port simultaneous read to separate banks test
+  rm.Reset();
+  rm.Preload(test_values, ram_module_size, false);
   for (int i = 0; i < NUM_PORTS; i++) {
     rm.ReadRequest(i << RAM_ADDRESS_WIDTH, i);
   }
@@ -55,11 +84,11 @@ int main (int argc, char** argv) {
   for (int i = 0; i < NUM_PORTS; i++) {
     assert(rm.ReadReady(i) == true);
     assert(rm.ReadData(i) == (i << RAM_ADDRESS_WIDTH));
-  }
-
+  }  
+  
   // Two ports read conflict test
   rm.Reset();
-  preload(&rm);
+  rm.Preload(test_values, ram_module_size, false);
   rm.ReadRequest(0, 0);
   rm.ReadRequest(1, 1);
   rm.NextClockCycle();
@@ -83,7 +112,7 @@ int main (int argc, char** argv) {
   
   // Non-starvation test
   rm.Reset();
-  preload(&rm);
+  rm.Preload(test_values, ram_module_size, false);
   rm.ReadRequest(0, 0);
   rm.ReadRequest(1, 1);
   rm.NextClockCycle();
@@ -146,7 +175,7 @@ int main (int argc, char** argv) {
 
   // Write then read test
   rm.Reset();
-  preload(&rm);
+  rm.Preload(test_values, ram_module_size, false);
   rm.ReadRequest(0, 0);
   rm.NextClockCycle();
   rm.WriteRequest(0, 0, (int) 0xDEADBEEF);
